19897c764de3ddce36d24d5fc17ecfa0f6bc228e
evelyn401 <evelynlin401@gmail.com>
2025-12-26 15:09:05 +0800

    feat: 在NavBar函數中新增MenuIDs陣列以支持導航項目識別

--- javascript/module/navbar.js
+++ javascript/module/navbar.js
@@ -2,6 +2,7 @@ export function NavBar(){
     // 宣告陣列，將元素存入陣列之中;陣列資料，讀取「索引值」，索引值從0開始
     let MenuItems = ["About", "Skill", "Works", "Contact"]; 
     let MenuLinks = ["#Abouts", "#Skills", "#Works", "#Contacts"];
+    let MenuIDs = ["NavAbout", "NavSkill", "NavWorks", "NavContact"];
 
     // 新增 HTML標籤，利用清單列舉資料 ul....li
     
