f31d7b41a677dd8ecf08ffeb86ced0a11b43f8c8
evelyn401 <evelynlin401@gmail.com>
2025-12-31 16:04:32 +0800

    feat: 修正 HTML 和 Vue 應用的 ID 大小寫，分開測試與正式版的 Vue 實例

--- vue/vue.js
+++ vue/vue.js
@@ -1,7 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import { BannerCarousel } from './module/Carousel.js'
 
-createApp({
+const app = createApp({
     data() {
         return {
             message: '<span style="color: red">This should be red.</span>',
@@ -22,6 +22,14 @@ createApp({
         }
     }
 }).mount('#app');
+
+const app2 = createApp({
+    data() {
+        return {
+            
+        }
+    }
+}).mount('#App');
 
 // 啟動廣告圖輪播功能
 BannerCarousel();
