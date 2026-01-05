7a231b73aaf030f7bc750af2728ced33b93fda93
evelyn401 <evelynlin401@gmail.com>
2026-01-05 15:06:37 +0800

    feat: 更新 This.js 檔案，為各個範例添加編號以增強可讀性

--- javascript/module/This.js
+++ javascript/module/This.js
@@ -1,8 +1,8 @@
-// 全域環境 在瀏覽器的環境之下，全域作用域的 this 指向 window 物件
+// 1.全域環境 在瀏覽器的環境之下，全域作用域的 this 指向 window 物件
 // 在Node.js的環境之下，全域作用域的 this 是gobalthis，但在頂層程式碼內this會是空物件{}
 console.log(this); 
 
-// 物件方法內的 this 指向該物件
+// 2.物件方法內的 this 指向該物件
 const obj = {
     name: 'EVE',
     greet(){
@@ -11,9 +11,25 @@ const obj = {
 };
 obj.greet(); // EVE
 
-// 建構函式內的 this 指向新建立的物件 使用 new 關鍵字呼叫建構函式時，this會指向新建立的物件
+// 3.建構函式內的 this 指向新建立的物件 使用 new 關鍵字呼叫建構函式時，this會指向新建立的物件
 function Person(name){
     this.name = name;
 }
 const person1 = new Person('Alice');
 console.log(person1.name); // Alice
+
+// 4.箭頭函式內的 this 繼承自外層作用域的 this
+const confession = {
+    message: "我喜歡妳！",
+    start: function() {
+        console.log("倒數開始...");
+
+    // 使用箭頭函式
+    setTimeout(() => {
+        // 這裡的 this 會繼承外層 start 函式的 this (即 confession 物件)
+            console.log("3... 2... 1...");
+            console.log(this.message); 
+        }, 3000); // 3秒後執行
+    }
+};
+confession.start();
