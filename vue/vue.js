005342b06eb7611c18bafaa8ec259d15f46b5736
evelyn401 <evelynlin401@gmail.com>
2026-02-11 14:17:48 +0800

    fix: 修正圖片顯示邏輯，確保選擇的圖片正確顯示

--- vue/vue.js
+++ vue/vue.js
@@ -1,27 +1,27 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import { BannerCarousel } from './module/Carousel.js'
 
-const app = createApp({
-    data() {
-        return {
-            message: '<span style="color: red">This should be red.</span>',
-            test:{
-                id: "colorTest",
-                class: "info",
-                style: { color: "blue" }
-            },
-            text: "我是字串",
-            IfText:true
-            // IfText:false
-        }
-    },
-    // 自訂函式內容
-    methods: {
-        ChangeText(){
-            this.IfText = !this.IfText;
-        }
-    }
-}).mount('#app');
+// const app = createApp({
+//     data() {
+//         return {
+//             message: '<span style="color: red">This should be red.</span>',
+//             test:{
+//                 id: "colorTest",
+//                 class: "info",
+//                 style: { color: "blue" }
+//             },
+//             text: "我是字串",
+//             IfText:true
+//             // IfText:false
+//         }
+//     },
+//     // 自訂函式內容
+//     methods: {
+//         ChangeText(){
+//             this.IfText = !this.IfText;
+//         }
+//     }
+// }).mount('#app');
 
 const app2 = createApp({
     data() {
